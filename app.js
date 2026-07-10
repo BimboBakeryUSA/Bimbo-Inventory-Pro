aaca80460b8030baa617cb537d3e5dda803d5ae1
BimboBakeryUSA <doug.morales.bimbo@gmail.com>
2026-07-09 23:03:31 -0400

    Add global error handler to app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 alert("APP.JS CARGADO");
-  
- 
+
+window.onerror = function(msg, src, line, col, err) {
+  alert(
+    "ERROR:\n" +
+    msg +
+    "\nLINEA: " + line
+  );
+};
+
+alert("DESPUES DEL ONERROR");
